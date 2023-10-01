1aff59eab203e57d2d175e18e5ac1373b899e11d
Ravinder badesra <rsbadesra3899@gmail.com>
2023-10-01 11:27:12 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:todoapp_crud/screens/homepage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+ 
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return   MaterialApp(home: HomePage(),debugShowCheckedModeBanner: false,);
+  }
+}
